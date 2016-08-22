5183b44b04da7369529c65837547dfa04e2c7fcd
Vikmar Sair Ruiz Vargas <Vikmar Sair Ruiz Vargas>
2016-08-22 14:20:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vikmar/taller_3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vikmar.taller_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculadora_table);
+    }
+}
